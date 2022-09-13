386e0a4fe5f6cdc6f6eab39c35ce805135d56e30
daniaoren <mini.luigi@gmail.com>
2022-09-13 18:10:26 +0800

    del<M-S-F3>:w Please enter the commit message for your changes. Lines starting

--- telegram_lib.py
+++ telegram_lib.py
@@ -1,9 +1,6 @@
 import config_telegram
 import telegram
 
-# api_key = '5282794877:AAHlAPZ6Nq0i3oUVMNfRCd_4pZ48Rep5C-M'
-# user_id = 'hometruck'
-
 bot = telegram.Bot(token=api_key)
 
 def sendMessage(msg):
